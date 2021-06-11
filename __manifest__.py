4b50dfc44aff0b8082d206a6d2702788b10a6ba7
Andre Angel <andre.angel@redytec.local>
2021-06-11 16:15:39 -0500

    Basics

--- __manifest__.py
+++ __manifest__.py
@@ -1,1 +1,16 @@
-
+{
+    'name': "Hospital Management",
+    'version': '14.0.1.0.0',
+    'summary': 'Hospital Management Software',
+    'sequence': 20,
+    'description': """Longer Description""",
+    'category': 'Productivity',
+    'website': 'http://www.redytec.com',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto:install': False,
+}
