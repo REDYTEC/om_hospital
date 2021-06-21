f8f4ec915862015f3c053a08c40398b9dec7f514
Andre Angel <andre.angel@redytec.local>
2021-06-21 17:51:57 -0500

    New model

---
+++ models/appointment.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _, tools
+
+
+class HospitalAppointment(models.Model):
+    _name = "hospital.appointment"
+    _inherit = ["mail.thread", 'mail.activity.mixin']
+    _description = "Hospital Appointment"
+
+    name = fields.Char(string='Order Reference', required=True, copy=False, redonly=True, default=lambda self:_('New'))
+    patient_id = fields.Many2one('hospital.patient', string="Patient's name", required=True)
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('confirm', 'Confirmed'),
+        ('done', 'Done'),
+        ('cancel', 'Cancelled')
+    ], string='Status', default='draft', readonly=True, tracking=True)
+    note = fields.Text(string='Description')
+
+    def action_confirm(self):
+        self.state = 'confirm'
+
+    def action_done(self):
+        self.state = 'done'
+
+    def action_draft(self):
+        self.state = 'draft'
+
+    def action_cancel(self):
+        self.state = 'cancel'
+
+    @api.model
+    def create(self, vals):
+        if not vals.get('note'):
+            vals['note'] = 'New Patient'
+        if vals.get('reference', _('New')) == _('New'):
+            vals['reference'] = self.env['ir.sequence'].next_by_code('hospital.appointment') or _('New')
+        return super(HospitalPatient, self).create(vals)
