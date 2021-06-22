f35d6cb496a1a07ffa222b94cb001e5b5364425c
Andre Angel <andre.angel@redytec.local>
2021-06-22 11:57:21 -0500

    adding related fields

--- models/appointment.py
+++ models/appointment.py
@@ -10,6 +10,11 @@ class HospitalAppointment(models.Model):
     name = fields.Char(string='Order Reference', required=True, copy=False, redonly=True, default=lambda self:_('New'))
     patient_id = fields.Many2one('hospital.patient', string="Patient's name", required=True)
     age = fields.Integer(string='Age', related='patient_id.age', tracking=True)
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+    ], string="Gender")
     state = fields.Selection([
         ('draft', 'Draft'),
         ('confirm', 'Confirmed'),
