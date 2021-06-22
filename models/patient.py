c5b9b50279c1dd56fcb8a2268441bdac8885ec15
Andre Angel <andre.angel@redytec.local>
2021-06-22 16:40:58 -0500

    help

--- models/patient.py
+++ models/patient.py
@@ -4,7 +4,6 @@ from odoo import api, fields, models, _, tools
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
-    _inherit = ["mail.thread", 'mail.activity.mixin']
     _description = "Hospital Patient"
 
     name = fields.Char(string='Name', required=True, tracking=True)
