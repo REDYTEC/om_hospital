759cd59597013fe58c43c03e451ef87ba4749fdb
Andre Angel <andre.angel@redytec.local>
2021-06-22 12:46:51 -0500

    deleting menu

--- __manifest__.py
+++ __manifest__.py
@@ -17,8 +17,7 @@
         'views/patient_view.xml',
         'views/sale.xml',
         'views/kids_view.xml',
-        'views/patient_gender_view.xml',
-        'views/appointment_view.xml'
+        'views/patient_gender_view.xml'
     ],
     'demo': [],
     'qweb': [],
