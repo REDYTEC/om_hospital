70f43a131aec453bce83c9880e3c6d25fea16237
Andre Angel <andre.angel@redytec.local>
2021-06-23 12:27:23 -0500

    adding menu

--- models/appointment.py
+++ models/appointment.py
@@ -15,6 +15,7 @@ class HospitalAppointment(models.Model):
                               ('done', 'Done'), ('cancel', 'Cancelled')], default='draft',
                              string='Status', tracking=True)
     note = fields.Text(string='Description', tracking=True)
+    date_appointment = fields.Date(string='Date')
 
     def action_confirm(self):
         self.state = 'confirm'
