7fa519ccf9c353eb88410e72c052466e55afbc3e
Andre Angel <andre.angel@redytec.local>
2021-07-02 12:31:40 -0500

    changes to wizard

--- wizard/create_appointment.py
+++ wizard/create_appointment.py
@@ -21,19 +21,6 @@ class CreateAppointmentWizard(models.TransientModel):
             'view_mode': 'form',
             'res_model': 'hospital.appointment',
             'res_id': appointment_rec.id,
-            'target': 'new',
+            # activate if want to appear as a pop up
+            # 'target': 'new',
         }
-        # vals = {
-        #     'patient_id': self.patient_id.id,
-        #     'date_appointment': self.date_appointment
-        # }
-        # appointment_rec = self.env['hospital.appointment'].create(vals)
-        # return {
-        #     'name': _('Appointment'),
-        #     'type': 'ir.actions.act_window',
-        #     'view_mode': 'form',
-        #     'res_model': 'hospital.appointment',
-        #     'red_id': appointment_rec.id,
-        #     # activate if want to appear as a pop up
-        #     'target': 'new',
-        # }
