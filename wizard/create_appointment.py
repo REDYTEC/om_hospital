625c09a667b920555fc965bc67f8f2c9fea11bd6
Andre Angel <andre.angel@redytec.local>
2021-07-02 16:04:54 -0500

    show things from wizard

--- wizard/create_appointment.py
+++ wizard/create_appointment.py
@@ -26,6 +26,7 @@ class CreateAppointmentWizard(models.TransientModel):
         }
 
     def action_view_appointment(self):
-        action = self.env.ref('om_hospital.action_hospital_appointment').read()[0]
-        action['domain'] = [('patient_id', '=', self.patient_id.id)]
+        # action = self.env.ref('om_hospital.action_hospital_appointment').read()[0]
+        # action['domain'] = [('patient_id', '=', self.patient_id.id)]
+        action = self.env["ir.actions.act_window"]._for_xml_id("om_hospital.action_hospital_appointment")
         return action
