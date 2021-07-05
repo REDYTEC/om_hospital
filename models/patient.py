07363dfd85d490387dcfb76a001f4528fd13d5b2
Andre Angel <andre.angel@redytec.local>
2021-07-05 16:30:48 -0500

    adding image

--- models/patient.py
+++ models/patient.py
@@ -19,6 +19,7 @@ class HospitalPatient(models.Model):
     note = fields.Text(string='Description', tracking=True)
     responsible_id = fields.Many2one('res.partner', string="Responsible")
     appointment_count = fields.Integer(string='Appointment Count', compute='_compute_appointment_count')
+    image = fields.Binary(string='Patient Image')
 
     def _compute_appointment_count(self):
         # para evitar problemas con que self tome varios valores a la vez, se pasa la funcion por un for
