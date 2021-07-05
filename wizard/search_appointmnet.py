fbba1775924899a8d6f6b2e8c0b4dfdc5a73eea8
Andre Angel <andre.angel@redytec.local>
2021-07-05 15:27:53 -0500

    changes

--- wizard/search_appointmnet.py
+++ wizard/search_appointmnet.py
@@ -7,3 +7,8 @@ class CreateAppointmentWizard(models.TransientModel):
     _description = 'Search Appointment Wizard'
 
     patient_id = fields.Many2one('hospital.patient', string='Patient', required=True)
+
+    def action_search_appointment_m1(self):
+        action = self.env["ir.actions.act_window"]._for_xml_id("om_hospital.action_hospital_appointment")
+        action['domain'] = [('patient_id', '=', self.patient_id.id)]
+        return action
