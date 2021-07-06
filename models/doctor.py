e4daf601effa2becd8d02096ebc17d66495a2a9a
Andre Angel <andre.angel@redytec.local>
2021-07-06 11:42:52 -0500

    new field in appointment

--- models/doctor.py
+++ models/doctor.py
@@ -6,6 +6,9 @@ class HospitalAppointment(models.Model):
     _name = "hospital.doctor"
     _inherit = ["mail.thread", 'mail.activity.mixin']
     _description = "Hospital Doctor"
+    # Because the model doesn't have a 'name' field, to get a name to a many2one field that uses this info,
+    # need to specify a main id with _rec_name
+    _rec_name = 'doctor_name'
 
     doctor_name = fields.Char(string='Name', required=True, tracking=True)
     age = fields.Integer(string='Age', tracking=True)
