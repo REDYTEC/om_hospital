cc864c047c2cbd5fd691d62952bf205ec60bb7fa
Andre Angel <andre.angel@redytec.local>
2021-07-20 10:17:43 -0500

    one2many

--- models/appointment.py
+++ models/appointment.py
@@ -67,6 +67,6 @@ class AppointmentPrescriptionLines(models.Model):
     _name = "appointment.prescription.lines"
     _description = "Appointment Prescription Lines"
 
-    name = fields.Char(string='Medicine')
+    name = fields.Char(string='Medicine', required=True)
     qty = fields.Integer(string='Quantity')
     appointment_id = fields.Many2one('hospital.appointment', string='Appointment')
